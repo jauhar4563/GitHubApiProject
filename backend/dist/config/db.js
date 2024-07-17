b0eb09bcb721ab73fdc818393e15414c11bd5b49
jauhar4563 <jauharp02@gmail.com>
2024-07-17 20:09:21 +0530

    postgres link

--- backend/dist/config/db.js
+++ backend/dist/config/db.js
@@ -7,7 +7,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const pg_1 = require("pg");
 const pool = new pg_1.Pool({
-    connectionString: process.env.POSTGRES_URL || "postgres://default:6BDikPgFHS3z@ep-shy-block-a48i3q72-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    connectionString: process.env.POSTGRES_URL,
 });
 pool.on('connect', () => {
     console.log('Connected to the database');
