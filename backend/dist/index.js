d6f1a5de6c3c15a719e31262178566373e8fe4db
jauhar4563 <jauharp02@gmail.com>
2024-07-17 16:54:02 +0530

    connection string dist

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -13,7 +13,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((req, res, next) => {
     console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-    next(); // Move to the next middleware or route handler
+    next();
 });
 app.use((0, cors_1.default)({
     origin: "*",
